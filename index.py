cce5692a79571affc7081a8193949bf9d6ee67bd
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2021-05-29 23:48:27 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('cartoon.jpg')
+
+#Edges
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 3)
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 11)
+
+#Color
+color = cv2.bilateralFilter(img, 9, 300, 300)
+
+#Final Cartoon
+final = cv2.bitwise_and(color, color, mask=edges)
+
+# print(image.shape)
+cv2.imshow('Image', img)
+cv2.imshow('Cartoon',final)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
